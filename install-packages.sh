d4fa918d4f565d2f5c4fcbca6d9e294f9b882d7c
adhuliya <dhuliya@cse.iitb.ac.in>
2020-03-10 17:14:01 +0530

    semi-automated-update

--- install-packages.sh
+++ install-packages.sh
@@ -3,42 +3,42 @@
 # install packages
 sudo apt-get update;
 
-sudo apt-get install -y vim-gtk3
-sudo apt-get install -y git
-sudo apt-get install -y tree
-sudo apt-get install -y zsh
-sudo apt-get install -y apt-file
-sudo apt-get install -y nginx
-sudo apt-get install -y apache2-utils
-sudo apt-get install -y recoll
-sudo apt-get install -y -f
-sudo apt-get install -y texlive-full texmaker
-sudo apt-get install -y gcc
-sudo apt-get install -y ninja-build
-sudo apt-get install -y cmake
-sudo apt-get install -y g++
-sudo apt-get install -y conky
-sudo apt-get install -y cython
-sudo apt-get install -y ipython
-sudo apt-get install -y ipython3
-sudo apt-get install -y xdot
-sudo apt-get install -y curl
-sudo apt-get install -y redshift
-sudo apt-get install -y python3-pip
-sudo apt-get install -y net-tools
-sudo apt-get install -y libreoffice
-sudo apt-get install -y recordmydesktop
-sudo apt-get install -y gtk-recordmydesktop
-sudo apt-get install -y vlc
-sudo apt-get install -y openshot
-sudo apt-get install -y vokoscreen
-sudo apt-get install -y espeak
-sudo apt-get install -y xsel
-sudo apt-get install -y ghc
-sudo apt-get install -y postgresql-11 pgadmin4
-sudo apt-get install -y autoconf automake libtool curl make g++ unzip
-sudo apt-get install -y cloc
-sudo apt-get install -y pypy3
-sudo apt-get install -y openssh-server
+sudo apt-get install -y vim-gtk3;
+sudo apt-get install -y git;
+sudo apt-get install -y tree;
+sudo apt-get install -y zsh;
+sudo apt-get install -y apt-file;
+sudo apt-get install -y nginx;
+sudo apt-get install -y apache2-utils;
+sudo apt-get install -y recoll;
+sudo apt-get install -y -f;
+sudo apt-get install -y texlive-full texmaker;
+sudo apt-get install -y gcc;
+sudo apt-get install -y ninja-build;
+sudo apt-get install -y cmake;
+sudo apt-get install -y g++;
+sudo apt-get install -y conky;
+sudo apt-get install -y cython;
+sudo apt-get install -y ipython;
+sudo apt-get install -y ipython3;
+sudo apt-get install -y xdot;
+sudo apt-get install -y curl;
+sudo apt-get install -y redshift;
+sudo apt-get install -y python3-pip;
+sudo apt-get install -y net-tools;
+sudo apt-get install -y libreoffice;
+sudo apt-get install -y recordmydesktop;
+sudo apt-get install -y gtk-recordmydesktop;
+sudo apt-get install -y vlc;
+sudo apt-get install -y openshot;
+sudo apt-get install -y vokoscreen;
+sudo apt-get install -y espeak;
+sudo apt-get install -y xsel;
+sudo apt-get install -y ghc;
+sudo apt-get install -y postgresql-11 pgadmin4;
+sudo apt-get install -y autoconf automake libtool curl make g++ unzip;
+sudo apt-get install -y cloc;
+sudo apt-get install -y pypy3;
+sudo apt-get install -y openssh-server;
 
 
